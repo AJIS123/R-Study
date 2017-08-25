438801d88fd4d44a896ffcf10445bacf86bba9ad
AJIS123 <aji.jan90@gmail.com>
2017-08-25 01:30:43 -0500

    Add files via upload

---
+++ Functions.R
@@ -0,0 +1,26 @@
+#? help
+
+?rnorm()
+
+rnorm(5)
+
+rnorm(5,10,8)
+
+rnorm(n=5,mean=10,sd=8)
+x<-c(1,2,3)
+seq(from =10,to=20,along.with = x)
+
+
+?rep()
+
+rep(1:5,5)
+rep(1:5,each=2)
+
+a<-c(10,20,115)
+?sqrt()
+sqrt(a)
+
+
+#packages
+qplot(data=diamonds,carat,price,color=clarity,facets=.~clarity)
+
